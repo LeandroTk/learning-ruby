e780505c298aa68a00d8d546e0798dbb3f5d39a7
Leandro Tk <leandrodtk@hotmail.com>
2017-03-18 00:56:00 -0300

    Ruby Looping: looping statement

---
+++ looping.rb
@@ -0,0 +1,21 @@
+num = 1
+
+while num <= 10
+  puts num
+  num += 1
+end
+
+num = 1
+
+until num > 10
+  puts num
+  num += 1
+end
+
+for num in 1...10
+  puts num
+end
+
+[1, 2, 3, 4, 5].each do |num|
+  puts num
+end
